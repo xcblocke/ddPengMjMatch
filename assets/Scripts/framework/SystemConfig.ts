e799158ede7841a09ff742ab71782050f77174b6
locke 徐 <1045604318@qq.com>
2026-04-28 17:09:34 +0800

    注释掉http相关的 字符串配置

--- assets/Scripts/framework/SystemConfig.ts
+++ assets/Scripts/framework/SystemConfig.ts
@@ -1,7 +1,7 @@
-export var WX_APP_ID = "wx1462584e6e03d093";
-export var GAME_NAME = "ddfc";
-export var CUSTOMER_SERVICE = "https://ddfc.starrevs.com/help_center?package_name=com.ddfc.yf";
-export var USER_AGREEMENT = "https://ddfc.starrevs.com/user?package_name=com.ddfc.yf";
-export var PRIVACY_AGREEMENT = "https://ddfc.starrevs.com/private?package_name=com.ddfc.yf";
-export var IOS_USER_AGREEMENT = "https://klxyx-api.fingerscloud.com/hfi/user?package_name=com.dskhappyios.klxyx";
-export var IOS_PRIVACY_AGREEMENT = "https://klxyx-api.fingerscloud.com/hfi/private?package_name=com.dskhappyios.klxyx";
+export var WX_APP_ID = "";
+export var GAME_NAME = "";
+export var CUSTOMER_SERVICE = "";
+export var USER_AGREEMENT = "";
+export var PRIVACY_AGREEMENT = "";
+export var IOS_USER_AGREEMENT = "";
+export var IOS_PRIVACY_AGREEMENT = "";
