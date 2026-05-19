564948523092a137eeef976111d85a0d04c0a785
locke 徐 <1045604318@qq.com>
2026-05-19 16:10:17 +0800

    屏蔽不需要的功能弹窗

--- assets/WordFrame/Scripts/Panel_DailyClearanceReward.ts
+++ assets/WordFrame/Scripts/Panel_DailyClearanceReward.ts
@@ -228,7 +228,8 @@ export default class Panel_DailyClearanceReward extends cc.Component {
     }
 
     static start(closeCB?: () => void): void {
-        FrameSDK.openWindow("Panel_DailyClearanceReward", { closeCB });
+        // FrameSDK.openWindow("Panel_DailyClearanceReward", { closeCB });
+        closeCB?.();
     }
 }
 
