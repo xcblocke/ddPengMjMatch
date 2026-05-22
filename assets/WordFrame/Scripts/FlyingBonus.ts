e8238637db6d4bbceba021a918c23c620f4e4aa5
locke 徐 <1045604318@qq.com>
2026-05-22 14:14:53 +0800

    frame预制体调整

--- assets/WordFrame/Scripts/FlyingBonus.ts
+++ assets/WordFrame/Scripts/FlyingBonus.ts
@@ -36,7 +36,7 @@ export default class FlyingBonus extends cc.Component {
             return;
         }
 
-        this.FlySp.setAnimation(0,FrameData.saveData.fly_free <= 0?"animation2":"animation",true);
+        this.FlySp.setAnimation(0,FrameData.saveData.fly_free <= 0?"ad":"free",true);
         const passLevel = FrameSDK.frameData.gameData.passLevel;
         if (passLevel < FrameData.FRAME_CONF.flyingBonusLevel) {
             return;
